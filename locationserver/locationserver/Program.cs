607408b974fa3eda7667e751ec1e3f151bb31565
Sijan Shreesh Rana <Sijan_Rana@hotmail.com>
2019-02-12 21:12:37 +0000

    Implementing Server Protocols
    -Error with HTTP 1.0 POST Request

--- locationserver/locationserver/Program.cs
+++ locationserver/locationserver/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +10,110 @@ namespace locationserver
 {
     class Program
     {
+        public static Dictionary<string, string> Location { get; set; }//Dictionary database
+
+        public static string name;
+        public static string newLocation;
+
         static void Main(string[] args)
         {
+            Console.WriteLine("\n\nSERVER: Running Server...");
+            RunServer();//Start running server
+        }
 
+        public static void RunServer()
+        {
+            TcpListener listener;
+            Socket connection;
+            NetworkStream socketStream;
+
+            InitialiseLocationDictionary();///intialise dictionary with test data
+
+            try
+            {
+                Console.WriteLine("\nSERVER: Listening...\n\n");
+                listener = new TcpListener(IPAddress.Any, 43);
+                listener.Start();//Start listening for client
+                while (true)
+                {
+                    connection = listener.AcceptSocket();//Accepts client connection
+                    socketStream = new NetworkStream(connection);
+                    Console.WriteLine("\nSERVER: Connection Received");
+                    DoRequest(socketStream);//PROCESS REQUEST BY CLIENT
+                    socketStream.Close();
+                    connection.Close();//Closes client connection
+                    Console.WriteLine("SERVER: Connection Closed");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n\nSERVER: " + e.ToString());
+            }
+        }
+
+        public static void DoRequest(NetworkStream socketStream)
+        {
+            try
+            {
+                socketStream.ReadTimeout = 1000;//READ TIMEOUT
+                socketStream.WriteTimeout = 1000;//WRITE TIMEOUT
+
+                Request currentRequest = new Request(socketStream);
+                Console.WriteLine(name + newLocation);
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n\nSERVER: Something went very wrong\nSERVER: " + e.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Creates Location dictionary
+        /// </summary>
+        public static void InitialiseLocationDictionary()
+        {
+            Location = new Dictionary<string, string>();
+            Location.Add("Sijan", "Cray Lab");
+            Location.Add("cssbct", "RB-336");
+        }
+
+        /// <summary>
+        /// Updates location from dictionary using name
+        /// </summary>
+        /// <param name="name">Dictionary key</param>
+        /// <param name="newLocation">Dictionary value</param>
+        /// <returns>New location</returns>
+        public static string UpdateLocation(string name, string newLocation)
+        {
+            //UPDATE
+
+            if (Location.ContainsKey(name))
+            {
+                Location.Remove(name);
+                Location.Add(name, newLocation.Trim('"'));
+                Console.WriteLine("SERVER: LOCATION Updated");
+                return newLocation;
+            }
+            Console.WriteLine("SERVER: LOCATION NOT FOUND");
+            return null;
+        }
+
+        /// <summary>
+        /// Retrieves location from dictionary using name
+        /// </summary>
+        /// <param name="name">Dictionary key</param>
+        /// <returns>Retrieved location</returns>
+        public static string RetrieveLocation(string name)
+        {
+            //LOOK_UP
+            if (Location.ContainsKey(name))
+            {
+                Console.WriteLine("SERVER: LOCATION Found");
+                return Location[name];
+            }
+            Console.WriteLine("SERVER: LOCATION NOT FOUND");
+            return null;
         }
     }
 }
--- locationserver/locationserver/Request.cs
+++ locationserver/locationserver/Request.cs
@@ -3,11 +3,127 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+
 
 namespace locationserver
 {
-    class Request
+    public class Request
     {
+        NetworkStream networkStream;
+
+        protected string ProtocolCommand = null;
+        protected string ProtocolType = null;
+        protected string ProtocolVersion = null;
+
+        public Request(NetworkStream networkStream)
+        {
+            this.networkStream = networkStream;
+            StreamReader streamReader = new StreamReader(networkStream);
+            ReadStream(streamReader);
+        }
+
+        public void ReadStream(StreamReader streamReader)
+        {
+            int tokenCount = 0;//Lines left to read
+            string firstToken = streamReader.ReadLine();//First line
+            Console.WriteLine("SERVER: " + firstToken);
+            if(firstToken.Split('/')[0] != null)
+            {
+                ProtocolType = "HTTP";
+                ProtocolCommand = firstToken.Split(' ')[0].Trim();//Protocol Command SET
+                if(ProtocolCommand == "PUT")
+                {
+                    ProtocolCommand = "POST";//HTTP PUT --> POST
+                }
+                if(firstToken.Split('/')[2] != null)
+                {
+                    ProtocolVersion = firstToken.Split('/')[2].Trim();//Protocol Version SET
+                    if(ProtocolVersion == "1.0")
+                    {
+                        Program.name = firstToken.Split(' ')[1].Split('/')[1];//HTTP 1.0 name SET
+                    }
+                }
+                else
+                {
+                    ProtocolVersion = "0.9";
+                    Program.name = firstToken.Split('/')[1].Trim();//HTTP 0.9 name SET
+                }
+            }
+            else
+            {
+                ProtocolType = "WHOIS";
+                if(firstToken.Split(' ')[1] != null)
+                {
+                    ProtocolCommand = "POST";
+                    Program.newLocation = firstToken.Split(' ')[1];//WHOIS newLocation SET
+                }
+                else
+                {
+                    ProtocolCommand = "GET";
+                }
+                Program.name = firstToken.Split(' ')[0].Trim();//WHOIS name SET;
+            }
+            if(ProtocolVersion != null && ProtocolVersion != "0.9")
+            {
+                //Lines to be 'ignored'
+                if(ProtocolCommand == "GET")
+                {
+                    if(ProtocolVersion == "1.0")
+                    {
+                        tokenCount = 1;//Additional line to be read
+                    }
+                    else if(ProtocolVersion == "1.1")
+                    {
+                        tokenCount = 2;//Two lines to be read
+                    }
+                    else
+                    {
+                        //PROTOCOL VERSION ERROR
+                    }
+                }
+                else//POST
+                {
+                    if (ProtocolVersion == "1.0")
+                    {
+                        tokenCount = 2;//Additional line to be read
+                    }
+                    else if (ProtocolVersion == "1.1")
+                    {
+                        tokenCount = 3;//Two lines to be read
+                    }
+                    else
+                    {
+                        //PROTOCOL VERSION ERROR
+                    }
+                }
+            }
+
+
+            ///html 1.0 doesnt go through this point
+            if(ProtocolType == "HTTP")
+            {
+                for (int i = 0; i < tokenCount; i++)//Tokens to be read
+                {
+                    streamReader.ReadLine();//SKIP LINES
+                }
+                if(ProtocolType == "POST")
+                {
+
+                    string lastLine = streamReader.ReadLine();
+                    if (ProtocolVersion == "1.1")
+                    {
+                        Program.name = lastLine.Split('&')[0].Split('=')[1];//HTTP 1.1 name SET
+                        Program.newLocation = lastLine.Split('&')[1].Split('=')[1];//HTTP 1.1 newLocation SET
+                    }
+                    else
+                    {
+                        Program.newLocation = lastLine.Trim();//HTTP 0.9 HTTP 1.0 newLocation SET
+                    }
+                }
+            }
+        }
 
     }
 }
